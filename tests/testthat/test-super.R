379f9788bf098a044e48203895a31a421d42e3fd
RobertZK <technoguyrob@gmail.com>
2015-02-28 18:37:36 -0600

    initial super method

---
+++ tests/testthat/test-super.R
@@ -0,0 +1,33 @@
+context("super") 
+
+test_that("it calls the parent method in a single example chain", {
+  calls <- integer(0)
+  function1 <- function() {
+    calls <<- c(calls, 1L)
+    invisible(NULL)
+  }
+
+  local({
+    function2 <- function() {
+      function1 <- function() {
+        calls <<- c(calls, 2L)
+        super()
+      }
+
+      function3 <- function() {
+        function1 <- function() {
+          calls <<- c(calls, 3L)
+          super()
+        }
+        function1()
+      }
+
+      function3()
+    }
+
+    function2()
+  })
+
+  expect_equal(calls, 3L:1L)
+})
+
